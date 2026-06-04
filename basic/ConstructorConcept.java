cbacfde6a2217abd0e559cf3aee92f6167349647
Sinchana41 <sinchanaan1234@gmail.com>
2026-06-04 20:48:01 +0530

    basic concepts

---
+++ basic/ConstructorConcept.java
@@ -0,0 +1,20 @@
+package basic;
+public class ConstructorConcept {
+    int id;
+    String name;
+
+    //Default constructor
+    public ConstructorConcept(){
+    }
+
+    //parameterized Constructor
+    public ConstructorConcept(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        ConstructorConcept concept = new ConstructorConcept(101,"Sinchana");
+        System.out.println(concept.id+" : "+concept.name);
+    }
+}
