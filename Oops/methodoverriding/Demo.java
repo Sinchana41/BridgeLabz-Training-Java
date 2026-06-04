6baed1f342d53e5c1984f5a3379f2de3794f38c3
Sinchana41 <sinchanaan1234@gmail.com>
2026-06-04 21:06:46 +0530

    Delete Oops directory

--- Oops/methodoverriding/Demo.java
+++
@@ -1,9 +0,0 @@
-package methodoverriding;
-
-public class Demo {   public static void main(String[] args) {
-        Car car = new Car(1100000, "green");
-        car.riding();
-        Duster duster =  new Duster(1000000, "Silver");
-        duster.riding();
-    }
-}
